3455c170ef64806f02c0e9e631033f4d3f0aa4db
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2025-01-29 04:10:14 +0200

    Building a Custom Structure Directive

--- src/app/auth/auth.directive.ts
+++ src/app/auth/auth.directive.ts
@@ -1,23 +1,32 @@
-import { Directive, effect, inject, input } from '@angular/core';
+import {
+  Directive,
+  effect,
+  inject,
+  input,
+  TemplateRef,
+  ViewContainerRef,
+} from '@angular/core';
+
 import { Permission } from './auth.model';
 import { AuthService } from './auth.service';
 
 @Directive({
   selector: '[appAuth]',
-  standalone: true
+  standalone: true,
 })
 export class AuthDirective {
-  userType = input.required<Permission>({alias: 'appAuth'});
+  userType = input.required<Permission>({ alias: 'appAuth' });
   private authService = inject(AuthService);
+  private templateRef = inject(TemplateRef);
+  private viewContainerRef = inject(ViewContainerRef);
 
-  constructor() { 
+  constructor() {
     effect(() => {
       if (this.authService.activePermission() === this.userType()) {
-        console.log('SHOW ELEMENT');
+        this.viewContainerRef.createEmbeddedView(this.templateRef);
       } else {
-        console.log('DO NOT SHOW ELEMENT');
+        this.viewContainerRef.clear();
       }
     });
   }
-
 }
